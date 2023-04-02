98b782f1f8fb6731068e80a1f366306aeeb7c843
vsa_user <kritik.sova@gmail.com>
2023-04-02 22:59:15 +0300

    -

--- ContactClass.cs
+++ ContactClass.cs
@@ -1,10 +1,10 @@
 public class ContactClass
 {
     public string firstName;
- /*   public string secondName;
+    public string secondName;
     public string thirdName;
     public string telephoneNumber;
     public string email;
-    public int index;*/
+    public int index;
 
 }
